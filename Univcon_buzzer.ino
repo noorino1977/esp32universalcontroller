569094386ffa3864e2667a61e6aa38017aecf6fa
NoorSwawan-YD1GSE <31297080+noorino1977@users.noreply.github.com>
2024-03-15 23:43:11 +0700

    Add files via upload

---
+++ Univcon_buzzer.ino
@@ -0,0 +1,12 @@
+int buzzer = 13;
+
+void setup() {
+   pinMode(buzzer, OUTPUT);
+}
+ 
+void loop() {
+  digitalWrite(buzzer, HIGH);    
+  delay(1000);                
+  digitalWrite(buzzer, LOW);     
+  delay(1000);                
+}
